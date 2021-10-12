9b7eca7d03b1bee8a3085c7f8eb761a224518b21
Sterben33 <nikita.gorbunov.1976@mail.ru>
2021-10-12 15:44:01 +0300

    initial commit

---
+++ Multiplication.sol
@@ -0,0 +1,24 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+
+contract Multiplication {
+
+    uint public mult = 1;
+
+    constructor() public checkOwnerAndAccept {
+        require(tvm.pubkey() != 0, 101);
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+    }
+
+    modifier checkOwnerAndAccept {
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+        _;
+    }
+    function multiply(uint multiplier) public checkOwnerAndAccept {
+        require(multiplier >= 1 && multiplier <= 10, 103);
+        mult *= multiplier;
+    }
+}
